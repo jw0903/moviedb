e892c34d2e0a86d97d369e9f054202fe82a9987e
lin559 <scau_ljw@126.com>
2017-08-13 16:32:55 +0800

    细节调整

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 import Element from 'element-ui';
-import 'element-ui/lib/theme-default/index.css'
-import 'normalize.css'
+import 'element-ui/lib/theme-default/index.css';
+import 'normalize.css';
 import router from './router';
-import store from '../store'
+import store from '../store';
 import VueResource from 'vue-resource';
 Vue.config.productionTip = false
 
