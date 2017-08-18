e5572c21d991a97a58990c156dd9825593e7ec6a
lin559 <scau_ljw@126.com>
2017-08-18 15:45:55 +0800

    删除多余文件

--- store/index.js
+++ store/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import moving from './moving/index.js'
 import easy from './easy.js'
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        moving,
         easy
     }
 })
